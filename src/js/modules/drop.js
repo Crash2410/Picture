ba5d230256b29efaaf459e01b1ca66228b2f531c
Artem <artem.xolod.90@gmail.com>
2020-11-02 18:06:34 +0300

    Реализация Drag & Drop

---
+++ src/js/modules/drop.js
@@ -0,0 +1,63 @@
+const drop = () => {
+    const fileInputs = document.querySelectorAll('[name="upload"]');
+
+    ['dragenter', 'gradleave', 'dragover', 'drop'].forEach(eventName => {
+        fileInputs.forEach(input => {
+            input.addEventListener(eventName, preventDefaults, false);
+        });
+    });
+
+    function preventDefaults(e) {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+    function highlight(item) {
+        item.closest('.file_upload').style.border = '5px solid yellow';
+        item.closest('.file_upload').style.backgroundColor = 'rgba(0,0,0, 0.7)';
+    }
+
+
+    function unHighlight(item) {
+        item.closest('.file_upload').style.border = 'none';
+
+        if (item.closest('.calc_form')) {
+            item.closest('.file_upload').style.backgroundColor = "#fff";
+        } else {
+            item.closest('.file_upload').style.backgroundColor = "#ededed";
+        }
+
+    }
+
+
+    ['dragenter', 'dragover'].forEach(eventName => {
+        fileInputs.forEach(input => {
+            input.addEventListener(eventName, () => {
+                highlight(input);
+            }, false);
+        });
+    });
+
+    ['gradleave', 'drop'].forEach(eventName => {
+        fileInputs.forEach(input => {
+            input.addEventListener(eventName, () => {
+                unHighlight(input);
+            }, false);
+        });
+    });
+
+    fileInputs.forEach(item => {
+        item.addEventListener('drop', (e) => {
+            item.files = e.dataTransfer.files;
+
+            let dots;
+            const arr = item.files[0].name.split('.');
+            arr[0].length > 5 ? dots = "..." : dots = '.';
+            const name = arr[0].substring(0, 6) + dots + arr[1];
+
+            item.previousElementSibling.textContent = name;
+        });
+    });
+};
+
+export default drop;
